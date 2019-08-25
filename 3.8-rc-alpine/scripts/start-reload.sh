c92141734c9273db432c69765c0dea41fedaf432
Stuart George <stuart@accentdesign.co.uk>
2019-08-25 08:18:54 +0100

    single scripts folder

--- 3.8-rc-alpine/scripts/start-reload.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-set -e
-
-HOST=${HOST:-0.0.0.0}
-PORT=${PORT:-80}
-LOG_LEVEL=${LOG_LEVEL:-info}
-
-# Start Uvicorn with live reload
-uvicorn --reload --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
--- 3.8-rc-alpine/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env sh
-set -e
-
-DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
-export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
-
-# Start Gunicorn
-exec gunicorn -k uvicorn.workers.UvicornWorker -c "$GUNICORN_CONF" "$APP_MODULE"
