dd03b15c8ab2e051bdb82fb10b72ffe84e47a97a
Dave Fuller <dave@accentdesign.co.uk>
2021-10-20 10:02:30 +0100

    Allowing Starlette to respect HTTPS headers

--- build/start.sh
+++ build/start.sh
@@ -3,6 +3,7 @@ set -e
 
 DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
 export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
+export FORWARDED_ALLOW_IPS="*"
 
 # Start Gunicorn
 exec gunicorn -k uvicorn.workers.UvicornWorker -c "$GUNICORN_CONF" "$APP_MODULE"
